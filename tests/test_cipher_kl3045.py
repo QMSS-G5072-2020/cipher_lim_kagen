e2682130584b442ef99530746d91c27185ff3b09
Kagen <kl3045@columbia.edu>
2020-11-16 14:56:16 +0800

    Added one unit test, and added pytest as dependency.

--- tests/test_cipher_kl3045.py
+++ tests/test_cipher_kl3045.py
@@ -1,5 +1,11 @@
 from cipher_kl3045 import __version__
 from cipher_kl3045 import cipher_kl3045
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_sentence_with_spaces():
+    result = cipher_kl3045.cipher('Bob hit the prince', 1)
+    expected = 'Cpc iju uif qsjodf' #expected result, should the test pass
+    assert result == expected
